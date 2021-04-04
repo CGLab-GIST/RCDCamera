df045697cbec8c91bf437a46c75872ccd61c22f9
Jino Park <pjessesco@gmail.com>
2021-04-04 20:51:49 +0900

    Replace pyexr to imageio in client example

--- client_example.py
+++ client_example.py
@@ -2,20 +2,22 @@ import socket
 import time
 
 import numpy as np
-import pyexr
+import imageio
 
 # iPad device's IP address
 HOST = '192.168.0.9'
 # Default port is 12345
 # https://github.com/pjessesco/RemoteCamera/blob/b65894e1ce73c8052b43922e02fa84aa0d5b1a5f/RemoteCam/ViewController.swift#L20
 PORT = 12345
-ADDR = (HOST,PORT)
-# Specified in 
+ADDR = (HOST, PORT)
+# Specified in
 # https://github.com/pjessesco/RemoteCamera/blob/b65894e1ce73c8052b43922e02fa84aa0d5b1a5f/RemoteCam/ViewController.swift#L30
 TAIL_UTF8 = "__TAIL_TAIL_TAIL__".encode('utf-8')
 
+
+# Request signal to the RCDCamera application and return received data
 def request(clientSocket, signal):
-    print("send signal : ",signal)
+    print("send signal : ", signal)
     clientSocket.send(signal.encode())
     print("receiving..")
 
@@ -26,31 +28,42 @@ def request(clientSocket, signal):
         data += clientSocket.recv(50000)
 
     print("whole data received : ", len(data))
-    
+
     # if not sleep app may crashes
     time.sleep(1)
 
     return data
 
+# PNG formatted image itself is returned from the RCDCamera.
 def get_rgb_image(clientSocket, filename):
     img_data = request(clientSocket, 'rgb')
+    img_data = img_data[:-len(TAIL_UTF8)]
     path = filename + '.png'
-    
+
     f = open(path, 'wb')
     f.write(img_data)
     f.close()
 
+# Depth array binary is returned from the RCDCamera.
+# You have to convert it to exr or other image format manually.
 def get_depth_image(clientSocket, filename):
     path = filename + '.exr'
     img_data = request(clientSocket, 'depth')
 
     array = np.frombuffer(img_data[:-len(TAIL_UTF8)], dtype=np.float32)
+
     # LiDAR scene depth resolution is 256*192 in iPad 12.9 4th-gen.
-    array = np.reshape(array, (192,256))
-    
-    pyexr.write(path, array)
+    array = np.reshape(array, (192, 256))
+    print(array.shape)
+
+    imageio.imwrite(path, array)
+
 
 if __name__ == '__main__':
+
+    # Download exr plugin for imageio
+    # https://imageio.readthedocs.io/en/stable/format_exr-fi.html?highlight=exr#exr-fi-ilm-openexr
+    imageio.plugins.freeimage.download()
 
     print("Connecting...")
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -64,9 +77,9 @@ if __name__ == '__main__':
     request(clientSocket, 'dummy')
     print("Dummy call ends")
 
-    for i in range(1, 3):
-        get_rgb_image(clientSocket, str(i))
-        get_depth_image(clientSocket, str(i))
+
+    get_rgb_image(clientSocket, str(1))
+    get_depth_image(clientSocket, str(1))
 
     print("Close socket")
     clientSocket.close()
