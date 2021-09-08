2b3ff57fd16205f18953941f96ebbbca37c16a4b
Jino Park <pjessesco@gmail.com>
2021-09-08 13:49:50 +0900

    Add IP/Port viewer

--- RemoteCam/RGBDViewController.swift
+++ RemoteCam/RGBDViewController.swift
@@ -18,6 +18,7 @@ class RGBDViewController: UIViewController {
     @IBOutlet weak var fovView: UITextView!
     @IBOutlet weak var temperatureView: UITextField!
     @IBOutlet weak var rgbModeButton: UIButton!
+    @IBOutlet weak var IPAddrView: UITextView!
     
     @IBOutlet weak var warnTextViewer: UITextView!
     
@@ -47,6 +48,8 @@ class RGBDViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
                 
+        IPAddrView.text = getIPAddress()+":12345"
+        
         UIApplication.shared.isIdleTimerDisabled = true
         
         session = ARSession()
@@ -151,6 +154,36 @@ class RGBDViewController: UIViewController {
                .first!.delegate as! SceneDelegate
         sceneDelegate.window?.rootViewController = nextVC
     }
+}
+
+// https://stackoverflow.com/a/56342010
+func getIPAddress() -> String {
+    var address: String?
+    var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+    if getifaddrs(&ifaddr) == 0 {
+        var ptr = ifaddr
+        while ptr != nil {
+            defer { ptr = ptr?.pointee.ifa_next }
+
+            guard let interface = ptr?.pointee else { return "" }
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                // wifi = ["en0"]
+                // wired = ["en2", "en3", "en4"]
+                // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
+
+                let name: String = String(cString: (interface.ifa_name))
+                if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+    }
+    return address ?? ""
 }
 
 extension RGBDViewController: ARSessionDelegate{
